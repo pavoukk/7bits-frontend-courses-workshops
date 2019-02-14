82ab1ff6256f52ece109de1a36293c21bce0411f
Mikhail Remmele <mikhail.remmele@7bits.it>
2019-02-14 14:02:53 +0600

    add task for workshop

--- workshop-5/src/index.js
+++ workshop-5/src/index.js
@@ -2,19 +2,21 @@ import './styles/main.scss';
 
 import indexTemplate from './pages/index/index.hbs';
 import articleTemplate from './components/article/article.hbs';
+import spinnerTemplate from './components/spinner/spinner.hbs';
+
+const urls = [
+  'data1.json',
+  'data2.json',
+  'data3.json',
+  'data4.json'
+];
 
 document.addEventListener("DOMContentLoaded", function() {
   const root = $('#root');
   root.append(indexTemplate());
   const content = $('.content');
 
-  fetch('api/data.json')
-    .then((response) => {
-      return response.json();
-    })
-    .then((result) => {
-      result.data.forEach((item) => {
-        content.append(articleTemplate(item));
-      })
-    })
+  /**
+   * Place your code here
+   */
 });
