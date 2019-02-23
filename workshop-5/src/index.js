e8be7b75b1fb998b139aeccdbaaed4e6c9527f33
Tatiana Fokina <tatianamezzanine@gmail.com>
2019-02-23 17:40:21 +0600

    6th workshop

--- workshop-5/src/index.js
+++ workshop-5/src/index.js
@@ -2,19 +2,44 @@ import './styles/main.scss';
 
 import indexTemplate from './pages/index/index.hbs';
 import articleTemplate from './components/article/article.hbs';
+// import spinnerTemplate from './components/spinner/spinner.hbs';
 
-document.addEventListener("DOMContentLoaded", function() {
-  const root = $('#root');
-  root.append(indexTemplate());
-  const content = $('.content');
+const urls = [
+    'api/data1.json',
+    'api/data2.json',
+    'api/data3.json',
+    'api/data4.json'
+];
 
-  fetch('api/data.json')
-    .then((response) => {
-      return response.json();
-    })
-    .then((result) => {
-      result.data.forEach((item) => {
-        content.append(articleTemplate(item));
-      })
-    })
+document.addEventListener("DOMContentLoaded", function () {
+    const root = $('#root');
+    root.append(indexTemplate());
+    const content = $('.content');
+
+    /**
+     * Place your code here
+     */
+    let promises = [];
+    urls.forEach(el => {
+        promises.push(fetch(el)
+            .then(response => {
+                return response.json();
+            })
+            .catch(error => {
+                console.log(error);
+            })
+        );
+    });
+
+    Promise.all(promises)
+        .then((result) => {
+            console.log(result);
+            result.forEach(el => {
+                el.data.forEach((item) => {
+                    content.append(articleTemplate(item));
+                })
+            });
+        }).catch(error => {
+        console.log(error);
+    });
 });
